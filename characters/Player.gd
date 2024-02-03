42934826f9c4bd934864675f683dd1d5dd08117b
sam <samuel.hernandez.514@my.csun.edu>
2024-02-03 03:41:56 -0800

    modified files

--- characters/Player.gd
+++ characters/Player.gd
@@ -4,14 +4,14 @@ var currPos = [0,0]
 
 func _input(event):
 	if event.is_action_pressed("ui_right"):
-		currPos[0] += 10
+		currPos[0] += 100
 	elif event.is_action_pressed("ui_left"):
-		currPos[0] -= 10
+		currPos[0] -= 100
 	elif event.is_action_pressed("ui_left"):
-		currPos[0] -= 10
+		currPos[0] -= 100
 	elif event.is_action_pressed("ui_up"):
-		currPos[1] -= 10
+		currPos[1] -= 100
 	elif event.is_action_pressed("ui_down"):
-		currPos[1] += 10
+		currPos[1] += 100
 					 
 	self.position = Vector2(currPos[0],currPos[1])
