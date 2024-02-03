66851eb34411ba17e6e357ef796f08f9b175dc27
amyktruongdev <amyktruongdev@gmail.com>
2024-02-03 15:48:16 -0800

    Skeleton start page with options button working

--- Interface/Title-Screen.gd
+++ Interface/Title-Screen.gd
@@ -3,17 +3,19 @@ extends Control
 
 func _on_new_game_pressed():
 	pass # Replace with function body.
-	get_tree().change_scene_to_file("res://test.tscn")
-
+	get_tree().change_scene_to_file("res://Levels/game_level.tscn")
+	
 
 func _on_load_pressed():
 	pass # Replace with function body.
+
+
+
 
 
 func _on_quit_pressed():
 	get_tree().quit()
 	
 
-
 func _on_options_pressed():
-	get_tree().change_scene_to_file("res://options_screen.tscn")
+	get_tree().change_scene_to_file("res://Interface/options_screen.tscn")
