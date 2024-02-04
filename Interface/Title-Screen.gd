ee91b577c5c7e69b188d7f21358f4d71df11891c
sam <samuel.hernandez.514@my.csun.edu>
2024-02-04 07:55:22 -0800

    for debugging
    scoring not printing to save file

--- Interface/Title-Screen.gd
+++ Interface/Title-Screen.gd
@@ -1,17 +1,19 @@
 extends Control
-
+@export var stars = "res://global.gd"
+var loaded_stars = 0
 var loaded_stage = "res://Levels/game_level.tscn"
 const SAVE_FILE = "user://save_file.save"
 func _load_file():
 	if FileAccess.file_exists(SAVE_FILE):
 		var f = FileAccess.open(SAVE_FILE, FileAccess.READ)
 		loaded_stage = f.get_line().strip_edges()
+		loaded_stars = int(f.get_line())
+		print('loaded stars: ',loaded_stars as int )
 		f.close()
 
 func _on_new_game_pressed():
 	pass # Replace with function body.
 	get_tree().change_scene_to_file("res://Levels/game_level.tscn")
-
 
 func _on_load_pressed():
 	_load_file()
--- coin.gd
+++ coin.gd
@@ -1,8 +1,12 @@
 extends Area2D
 
 @onready var player_vars = get_node("/root/PlayerVariables")
-
+@onready var twinklesparkle = $twinklesparkle
 func _on_body_entered(_body):
+	
 	player_vars.coins_collected += 1
+	print('player_vars: ', player_vars)
+	twinklesparkle.play()
+	await get_tree().create_timer(1).timeout
 	print(player_vars.coins_collected)
 	queue_free()	
