2e7c8d169c547058e3ab17e6849883a852e2dd69
sam <samuel.hernandez.514@my.csun.edu>
2024-02-04 02:55:17 -0800

    updated collisions

--- characters/player2.gd
+++ characters/player2.gd
@@ -4,8 +4,9 @@ extends CharacterBody2D
 
 @export var move_speed : float = 300
 @onready var doorslam = $doorslam
-
+@onready var walk = $walk
 func _physics_process(_delta):
+	var walking = false
 	var x_axis = 0
 	var y_axis = 0
 	if Input.is_action_just_pressed("right"):
@@ -20,7 +21,12 @@ func _physics_process(_delta):
 	var input_direction = Vector2(x_axis, y_axis)
 	
 	velocity = input_direction * move_speed
-	
-	if move_and_collide(input_direction):
+	move_and_collide(input_direction)
+	if x_axis != 0:
+		walk.play()
+	elif move_and_collide(input_direction) && y_axis != 0:
 		doorslam.play()
-		
+	elif move_and_collide(input_direction) && x_axis != 0:
+		doorslam.play()
+	elif y_axis != 0:
+		walk.play()
