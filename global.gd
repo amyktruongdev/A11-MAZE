f6b45405b0d0ee66a1c777f456ac2b81a794cf0b
BrettBass <BrettBass@github.com>
2024-02-04 06:22:14 -0800

    save state finished

--- global.gd
+++ global.gd
@@ -1,12 +1,14 @@
 extends Node
 
 
-
+const SAVE_FILE = "user://save_file.save"
+var loaded_stage = "res://Levels/game_level.tscn"
 
 
 # Called when the node enters the scene tree for the first time.
 
 func _ready():
+	get_tree().set_auto_accept_quit(false)
 	var f = FileAccess.open('res://apiKey.env',FileAccess.READ)
 	var apiKey=f.get_line().strip_edges()
 	f.close()
@@ -29,3 +31,18 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+func _save_to_file():
+	var f = FileAccess.open(SAVE_FILE,FileAccess.WRITE)
+	f.store_string(get_tree().current_scene.scene_file_path)
+	f.close()
+	
+
+	#print(get_tree().current_scene.scene_file_path)
+	
+func _notification(what):
+	if what == NOTIFICATION_WM_CLOSE_REQUEST:
+		print(loaded_stage)
+		_save_to_file()
+		get_tree().quit()
+
