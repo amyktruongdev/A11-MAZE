71acf4da9584e758c680a12c7f6a6f33e495fa01
Connie <138333893+conniechen651@users.noreply.github.com>
2024-02-03 17:34:45 -0800

    Update tree collision so that only player can collide with tree

--- tree.gd
+++ tree.gd
@@ -1,7 +1,6 @@
 extends Area2D
 
-
-
-func _on_body_entered(_body):
-	print ("u hit the tree")
+func _on_body_entered(body):
+	if body.name == "player2":
+		print ("u hit the tree")
 	#insert script here
