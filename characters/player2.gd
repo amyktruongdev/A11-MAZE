335f0373cc8ba85692b2fce1544f0c4e1f3bfb51
sam <samuel.hernandez.514@my.csun.edu>
2024-02-04 05:21:05 -0800

    working level 1 and player

--- characters/player2.gd
+++ characters/player2.gd
@@ -10,23 +10,21 @@ func _physics_process(_delta):
 	var x_axis = 0
 	var y_axis = 0
 	if Input.is_action_just_pressed("right"):
-		x_axis += 50
+		x_axis += 25
 	elif Input.is_action_just_pressed("left"):
-		x_axis -= 50
+		x_axis -= 25
 	if Input.is_action_just_pressed("up"):
-		y_axis -= 50
+		y_axis -= 25
 	elif Input.is_action_just_pressed("down"):
-		y_axis += 50
+		y_axis += 25
 	
 	var input_direction = Vector2(x_axis, y_axis)
 	
 	velocity = input_direction * move_speed
 	move_and_collide(input_direction)
-	if x_axis != 0:
-		walk.play()
-	elif move_and_collide(input_direction) && y_axis != 0:
+	if move_and_collide(input_direction) && y_axis != 0:
 		doorslam.play()
 	elif move_and_collide(input_direction) && x_axis != 0:
 		doorslam.play()
-	elif y_axis != 0:
+	elif y_axis != 0 || x_axis != 0:
 		walk.play()
