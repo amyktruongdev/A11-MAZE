c299affd7b00d340e2396333b06756dfa80204b4
sam <samuel.hernandez.514@my.csun.edu>
2024-02-04 11:59:17 -0800

    working save and load

--- global.gd
+++ global.gd
@@ -51,8 +51,9 @@ func _save_to_file():
 	
 func _notification(what):
 	if what == NOTIFICATION_WM_CLOSE_REQUEST:
+		
 		print(loaded_stage)
-		_save_to_file()
+		await _save_to_file()
 		get_tree().quit()
 
 func _input(event):
