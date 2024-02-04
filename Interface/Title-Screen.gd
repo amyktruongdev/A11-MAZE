0f40b51520fd92573dd83fd67961dc68ce4459ff
amyktruongdev <amyktruongdev@gmail.com>
2024-02-03 18:22:22 -0800

    Button TTS when hovered

--- Interface/Title-Screen.gd
+++ Interface/Title-Screen.gd
@@ -10,12 +10,26 @@ func _on_load_pressed():
 	pass # Replace with function body.
 
 
-
-
-
 func _on_quit_pressed():
 	get_tree().quit()
 	
 
 func _on_options_pressed():
 	get_tree().change_scene_to_file("res://Interface/options_screen.tscn")
+
+
+
+func _on_new_game_mouse_entered():
+	$newgamesound.play()
+
+
+func _on_load_mouse_entered():
+	$loadgamesound.play()
+
+
+func _on_options_mouse_entered():
+	$optiongamesound.play()
+
+
+func _on_quit_mouse_entered():
+	$quitgamesound.play()
--- Interface/options_screen.gd
+++ Interface/options_screen.gd
@@ -3,3 +3,15 @@ extends Control
 
 func _on_back_pressed():
 	get_tree().change_scene_to_file("res://Interface/title_screen.tscn")
+
+
+func _on_back_mouse_entered():
+	$backgamesound.play()
+
+
+func _on_resize_gui_mouse_entered():
+	$resizeguigamesound.play()
+
+
+func _on_volume_mouse_entered():
+	$volumegamesound.play()
